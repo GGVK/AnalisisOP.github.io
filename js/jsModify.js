5243d7db78032eda6a02a7c68937aaa80e6d1bc5
Gabriel Garcia Vazquez <30303166+GGVK@users.noreply.github.com>
2023-02-04 13:48:38 -0600

    Update jsModify.js

--- js/jsModify.js
+++ js/jsModify.js
@@ -1,1 +1,15 @@
+function vinculo(){
+	window.open('http://www.google.com.mx','640x480','toolbar=no,status=no,scrollbars=no,location=no,menubar=no,directories=no,width=640,height=480');
+}
+	
+document.addEventListener("contextmenu", function(event){
+        event.preventDefault();
+    }, false);
 
+document.addEventListener("copy", function(event){
+    // Change the copied text if you want
+    event.clipboardData.setData("text/plain", "No se permite copiar en esta página web");
+    
+    // Prevent the default copy action
+    event.preventDefault();
+}, false);
