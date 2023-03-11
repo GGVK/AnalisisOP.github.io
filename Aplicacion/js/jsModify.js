14916249571d12b9ea099e6def366805bd6e958c
Gabriel Garcia Vazquez <30303166+GGVK@users.noreply.github.com>
2023-03-11 15:35:09 -0600

    Update jsModify.js

--- Aplicacion/js/jsModify.js
+++ Aplicacion/js/jsModify.js
@@ -3,7 +3,8 @@ function vinculo(){
 }
 
 function fillData(){
-	var ruta="https://app.svetlananalytics.com/app/project/dfdc43d9-d7e6-46a0-9a12-8ba44a21326a/shared_dashboard/export_SvetlanrossAnalytics_xLw38DBV.html#/SHARED_DASHBOARD#co=project&cid=dfdc43d9-d7e6-46a0-9a12-8ba44a21326a&psid=export_SvetlanrossAnalytics_xLw38DBV.html&keyid=undefined&data=eyJyIjp7ImEiOnsiYWN0aXZlUGFnZUlkIjoiNzI2NWExNmQtOTk2Ny00Y2E2LWI3MDYtMDQ2NDk2YzhlZmEzIn0sImkiOiJTSEFSRURfREFTSEJPQVJEIiwicyI6W119fQ==";
+	//var ruta="https://app.svetlananalytics.com/app/project/dfdc43d9-d7e6-46a0-9a12-8ba44a21326a/shared_dashboard/export_SvetlanrossAnalytics_xLw38DBV.html#/SHARED_DASHBOARD#co=project&cid=dfdc43d9-d7e6-46a0-9a12-8ba44a21326a&psid=export_SvetlanrossAnalytics_xLw38DBV.html&keyid=undefined&data=eyJyIjp7ImEiOnsiYWN0aXZlUGFnZUlkIjoiNzI2NWExNmQtOTk2Ny00Y2E2LWI3MDYtMDQ2NDk2YzhlZmEzIn0sImkiOiJTSEFSRURfREFTSEJPQVJEIiwicyI6W119fQ==";
+	var ruta="https://app.svetlananalytics.com/app/project/dfdc43d9-d7e6-46a0-9a12-8ba44a21326a/shared_dashboard/export_SvetlanrossAnalytics_6D5EVqJ1.html#/SHARED_DASHBOARD#co=project&cid=dfdc43d9-d7e6-46a0-9a12-8ba44a21326a&psid=export_SvetlanrossAnalytics_6D5EVqJ1.html&keyid=undefined&data=eyJyIjp7ImEiOnsiYWN0aXZlUGFnZUlkIjoiYjAyZmM1YzItYmY0ZS00ZGUyLWI3OGQtNzliOGVmMTA1MTEzIn0sImkiOiJTSEFSRURfREFTSEJPQVJEIiwicyI6W119fQ==";
 	var iframe = document.getElementById("frameIndex");
 	iframe.setAttribute("src", ruta);
 }
